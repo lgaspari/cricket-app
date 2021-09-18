1ac71909f9c235138218107d7dfc7b6b037a72b2
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-09-18 00:12:48 -0300

    feat: add Cricket screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,18 @@
-import logo from '../images/logo.svg';
-import './App.css';
+const cricketAudioPath = process.env.PUBLIC_URL + '/assets/audios/cricket.mp3';
+const cricketImgPath = process.env.PUBLIC_URL + '/assets/images/cricket.svg';
+
+const cricket = new Audio(cricketAudioPath);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      id="cricket-screen"
+      onClick={() => cricket.play()}
+      role="button"
+      style={{
+        backgroundImage: `url('${cricketImgPath}')`,
+      }}
+    />
   );
 }
 
