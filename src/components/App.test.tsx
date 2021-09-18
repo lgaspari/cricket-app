e41f7c39933f3281799846232a577bed7550bd05
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-09-18 00:12:48 -0300

    feat: add Cricket screen

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+// mocks
+window.HTMLMediaElement.prototype.play = jest.fn();
+
+test('can play cricket sound', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(screen.getByRole('button'));
+  expect(window.HTMLMediaElement.prototype.play).toHaveBeenCalled();
 });
